d1fa2b9d0493a4d2c397b5e765b367ec16ca8a05
minho Seo <14.73oo6o19@gmail.com>
2019-01-02 09:46:55 +0900

    D01 | 190102 start | 파일정리

--- startcamp/day05/send_message.py
+++ startcamp/day05/send_message.py
@@ -3,7 +3,7 @@ import requests
 
 #token='784700304:AAFl1zcLZDv4JRWjaTl-mAzReT8fJl8kMuA'
 #chat_id='718060199'
-token = os.getenv('TELEGRAM_BOT_TOKE')
+token = os.getenv('TELEGRAM_BOT_TOKEN')
 chat_id = os.getenv('CHAT_ID')
 
 text='yo!'
