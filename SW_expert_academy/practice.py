244bfc56582bd985c99b81d73f3a999fd1fa0337
MH Seo <14.73oo6o19@gmail.com>
2019-01-24 17:54:12 +0900

    D17 | 190124 | Django

--- SW_expert_academy/practice.py
+++ SW_expert_academy/practice.py
@@ -1,14 +1,6 @@
-def collatz(x):
-    count=0
-    while x>=1:
-        count+=1
-        if count!=500:
-            if x%2:
-                x*3+1
-            else:
-                x/2
-        else:
-            count=-1
-    return count
+a=3
+print(isinstance(a,int)) #T
+print(isinstance(a,float)) #F
+print(type(a)==int) #T
+print(a==3) #T
 
-print(collatz(6))
