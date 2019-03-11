f436a67a53bd82d900d1a71056099ef1cd2e3020
Minho Seo <14.73oo6o19@gamail.com>
2019-03-11 23:18:33 +0900

    D47 | 190311 | linked & sort

--- algorithm/insert_sort.py
+++ algorithm/insert_sort.py
@@ -1,12 +1,13 @@
 def insert(datas,n):
-    for j in range(n):
+    for j in range(n-1,-1,-1):
         target = datas[j]
         i = j-1
-        while i>0 and datas[i]>target:
+        while i>=0 and datas[i]>target:
             datas[i+1] = datas[i]
             i = i-1
         datas[i+1] = target
 
 datas = [1,9,3,6,7,0,4,9,5,5]
-insert(datas,1)
+for i in range(len(datas)):
+    insert(datas,len(datas))
 print(datas)
--- algorithm/요세푸스_linked.py
+++ algorithm/요세푸스_linked.py
@@ -3,8 +3,24 @@ class node:
         self.data = data
         self.link = link
 
+def josephus(item):
+    global head
+    newnode = node(item)
+    if head == None:
+        head = newnode
+    else:
+        p = head # -> node(1)
+        while p.link:
+            p = p.link
+        p.link = newnode
+
 
 input = 41
+head = None
+for i in range(1,input+1):
+    josephus(i)
 
-for i in range(input,-1,-1):
-    node(i)
+p = head
+while p:
+    print(p.data)
+    p = p.link
