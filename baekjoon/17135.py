db7e57114e13cfb0e62c34e4f33c192efbf1e964
Minho Seo <14.73oo6o19@gamail.com>
2019-04-15 09:00:34 +0900

    입사시험

--- baekjoon/17135.py
+++ baekjoon/17135.py
@@ -39,32 +39,32 @@ def combination(n,r,i,c):
 def issafe(y,x):
     return 0<=y<sero and 0<=x<garo
 
-def kill(y,x,k): #지금은 k가 문제의 원인, 그리고 dist도 이용하기
-    global now_d, next_d
-    if target[y-1][x]:
-        result.append((y-1,x))
+def kill(y,x,dist): #지금은 k가 문제의 원인, 그리고 dist도 이용하기
+    global flag
+    if target[y][x]:
+        result.append((y,x))
         return
-    distance[now_d] = 1
-    q.append((y-1,x))
-    while q!=[] and k==0:
-        val = q.popleft()
-        y,x = val[0],val[1]
+    visited[y][x] = 1
+    q.append((y,x))
+    while q and flag==0 and visited[y][x]<=dist:
+        y,x = q.popleft()
+        # print(y,x)
+        if not visited[y][x]+1<=dist:
+            return
+
         for delta in range(3):
             new_y = y+dy[delta]
             new_x = x+dx[delta]
-            if issafe(new_y,new_x) and not visited[new_y][new_x]:
+            if issafe(new_y,new_x) and not visited[new_y][new_x]: #and visited[y][x]+1<=dist
                 if target[new_y][new_x]:
-                    q.append((new_y,new_x))
-                    k = 1
+                    result.append((new_y,new_x))
+                    flag = 1
                     break
-                visited[new_y][new_x] = 1
-                next_d += 1
-                distance[next_d] = distance[now_d] + 1
-        now_d += 1
+                visited[new_y][new_x] = visited[y][x]+1
+                q.append((new_y,new_x))
 
 
-
-
+# 인풋받고 베이스데이터 만들기
 sero,garo,dist = map(int,input().split())
 datas = [[0]*garo for _ in range(sero)]
 for row in range(sero):
@@ -72,39 +72,152 @@ for row in range(sero):
 # print(datas)
 # target = copy.deepcopy(datas)
 
-n = [loca for loca in range(garo)]
+#조합 함수에 넣기위한 데이터
+n = [loca for loca in range(garo)] #가로줄 인덱스 리스트
 # print(n)
 comb = []
 c = []
 combination(n,3,0,c)
 # print(comb)
 
+#kill함수에 넣기위한 데이터
 dy = [0,-1,0]
 dx = [-1,0,1]
 visited = [[0]*garo for _ in range(sero)]
-distance = [0]*100
-now_d = next_d = 0
 q = collections.deque()
-result = []
+dq = collections.deque()
 ans = 0
 
 
 for loca_x in comb:
     target = copy.deepcopy(datas)
     result = []
-    for loca_y in range(sero,0,-1):
+    for loca_y in range(sero-1,-1,-1): #공수 위로 전진하기 전
         for killer in range(3):
+            flag = 0
             visited = [[0] * garo for _ in range(sero)]
-            distance = [0] * 100
-            now_d = next_d = 0
-            q = collections.deque()
+            q = dq
             kill(loca_y,loca_x[killer],dist)
         result = list(set(result))
+        # print(result)
         for ty,tx in result:
             target[ty][tx] = 0
         # print(target)
+    # print(result)
     if ans < len(result):
         ans = len(result)
 print(ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import collections
+# import copy
+#
+#
+# def combination(n,r,i,c):
+#     if r == 0:
+#         comb.append(c)
+#         return
+#     if i<len(n):
+#         combination(n,r-1,i+1,c+[n[i]])
+#         combination(n,r,i+1,c)
+#
+# def issafe(y,x):
+#     return 0<=y<sero and 0<=x<garo
+#
+# def kill(y,x,k): #지금은 k가 문제의 원인, 그리고 dist도 이용하기
+#     global now_d, next_d
+#     if target[y-1][x]:
+#         result.append((y-1,x))
+#         return
+#     distance[now_d] = 1
+#     q.append((y-1,x))
+#     while q!=[] and k==0:
+#         val = q.popleft()
+#         y,x = val[0],val[1]
+#         for delta in range(3):
+#             new_y = y+dy[delta]
+#             new_x = x+dx[delta]
+#             if issafe(new_y,new_x) and not visited[new_y][new_x]:
+#                 if target[new_y][new_x]:
+#                     q.append((new_y,new_x))
+#                     k = 1
+#                     break
+#                 visited[new_y][new_x] = 1
+#                 next_d += 1
+#                 distance[next_d] = distance[now_d] + 1
+#         now_d += 1
+#
+#
+#
+#
+# sero,garo,dist = map(int,input().split())
+# datas = [[0]*garo for _ in range(sero)]
+# for row in range(sero):
+#     datas[row] = list(map(int,input().split()))
+# # print(datas)
+# # target = copy.deepcopy(datas)
+#
+# #조합 함수에 넣기위한 데이터
+# n = [loca for loca in range(garo)] #가로줄 인덱스 리스트
+# # print(n)
+# comb = []
+# c = []
+# combination(n,3,0,c)
+# # print(comb)
+#
+# #kill함수에 넣기위한 데이터
+# dy = [0,-1,0]
+# dx = [-1,0,1]
+# visited = [[0]*garo for _ in range(sero)]
+# distance = [0]*100
+# now_d = next_d = 0
+# q = collections.deque()
+# result = []
+# ans = 0
+#
+#
+# for loca_x in comb:
+#     target = copy.deepcopy(datas)
+#     result = []
+#     for loca_y in range(sero,0,-1):
+#         for killer in range(3):
+#             visited = [[0] * garo for _ in range(sero)]
+#             distance = [0] * 100
+#             now_d = next_d = 0
+#             q = collections.deque()
+#             kill(loca_y,loca_x[killer],dist)
+#         result = list(set(result))
+#         for ty,tx in result:
+#             target[ty][tx] = 0
+#         # print(target)
+#     if ans < len(result):
+#         ans = len(result)
+# print(ans)
 
 
