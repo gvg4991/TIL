47b9df7c10e599be29e304b7215f24b7f82d8200
Minho Seo <14.73oo6o19@gamail.com>
2020-03-21 18:23:04 +0900

    study

--- jupyter/selenium_auto.py
+++ jupyter/selenium_auto.py
@@ -60,7 +60,9 @@ pause(2)
 import time
 time.sleep(2)
 
-6. 창 숨김 모드
+
+#추가 기능
+1. 창 숨김 모드
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 options.add_argument("--disable-gpu")
@@ -68,7 +70,7 @@ options.add_argument("lang=ko_KR")
 options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
 driver = webdriver.Chrome(chrome_options=options) #크롬드라이버 실행 시, 옵션 넣어주기
 
-7. 파일 다운로드 위치 설정
+2. 파일 다운로드 위치 설정
 options.add_experimental_option("prefs", {
   "download.default_directory": "설치 경로",
   "download.prompt_for_download": False,
@@ -76,19 +78,64 @@ options.add_experimental_option("prefs", {
   "safebrowsing.enabled": False
 })
 driver = webdriver.Chrome(chrome_options=options) #크롬드라이버 실행 시, 옵션 넣어주기
+
+3. 실행파일 생성
+pip install pyinstaller
+>> pyinstaller 파일명.py
+>> pyinstaller --onefile --noconsole 파일명.py
+>> pyinstaller -F -w 파일명.py
+- 아이콘 넣기
+>> pyinstaller icon='아이콘명.ico' 파일명.py
+
+4. 로깅
+참고: https://inma.tistory.com/136
+import logging
+logging.basicConfig(level = logging.ERROR) #기본이 ERROR
+logging.error('에러 로그')
+- 로그 파일 저장
+logging.basicConfig(filename='C:\\Users/ahnlab_auto_download.log', level=logging.DEBUG)
+- stream 및 file에 로그
+logger = logging.getLogger(__name__)
+streamhandler = logging.StreamHandler()
+filehandler = logging.FileHandler('C:\\Users/ahnlab_auto_download.log')
+logger.addHandler(streamhandler) #logger에 handler 설정
+logger.addHandler(filehandler)
+logger.error('SETTING')
+- 로그 형식 지정
+formatter = logging.Formatter('[%(asctime)s][%(levelname)s | %(filename)s: %(lineno)s] >> %(message)s' )
+streamhandler.setFormatter(formatter) #logger에 formatter 설정
+filehandler.setFormatter(formatter)
+logger.addHandler(streamhandler)
+logger.addHandler(filehandler)
+- 로그 파일 분할
+filemaxbyte = 1024 * 1024 * 100
+filehandler = logging.handlers.RotatingFileHandler(filename, maxBytes=filemaxbyte, backupCount=10)
+
 '''
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys 
 from selenium.webdriver.common.action_chains import ActionChains
 import time
+import logging
+
+
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('[%(asctime)s] %(message)s' )
+streamhandler = logging.StreamHandler()
+filehandler = logging.FileHandler('C:\\Users/ahnlab_auto_download.log')
+streamhandler.setFormatter(formatter)
+filehandler.setFormatter(formatter)
+logger.addHandler(streamhandler)
+logger.addHandler(filehandler)
+
+options = webdriver.ChromeOptions()
 
 # Headless (창숨김) 모드
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-options.add_argument("--disable-gpu")
-options.add_argument("lang=ko_KR")
-options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+# options.add_argument('headless')
+# options.add_argument("--disable-gpu")
+# options.add_argument("lang=ko_KR")
+# options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
 
 #파일 다운로드 위치 설정
 options.add_experimental_option("prefs", {
@@ -97,30 +144,31 @@ options.add_experimental_option("prefs", {
   "download.directory_upgrade": True,
   "safebrowsing.enabled": False
 })
-print('SETTING')
+logger.error('SETTING')
 
 #안랩 접속
 driver = webdriver.Chrome(chrome_options=options)
 driver.implicitly_wait(20)
-print('CHROME')
+logger.error('CHROME')
 url = 'https://www.ahnlab.com/kr/site/login/loginForm.do'
 driver.get(url)
 driver.implicitly_wait(20)
-print('AHNLAB')
+time.sleep(1)
+logger.error('AHNLAB')
 
 #로그인
 driver.find_element_by_css_selector('#userId').send_keys('csmpower')
 driver.find_element_by_css_selector('#passwd').send_keys('semes12_ahnlab')
 driver.find_element_by_css_selector('#passwd').send_keys(Keys.ENTER)
 driver.implicitly_wait(2)
-print('LOGIN')
+logger.error('LOGIN')
 
 #엔진파일 다운로드 페이지 이동
 driver.get('https://www.ahnlab.com/kr/site/download/product/productEngineList.do')
 
 #v3 통합엔진 설치
 driver.find_element_by_xpath('//*[@id="form"]/div[3]/table/tbody/tr[1]/td[4]/a').click()
-print('V3 DOWNLOAD')
+logger.error('V3 DOWNLOAD')
 time.sleep(2)
 
 # #다운로드 관리 페이지
